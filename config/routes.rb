509e2f2de0e458ebd61678d6484fb39f1da4b69c
Yeng <leeyeng2005@gmail.com>
2017-06-28 00:52:13 -0500

    Set up user stocks along associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :user_stocks, except: [:show, :edit, :update]
   root to: 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
